35d41d1759a694a0ac18619e92b4ea1d40575c9b
cokkike88 <cok_eight@hotmail.com>
2017-12-15 13:05:18 -0600

    Cambio

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', function(req, res){
 });
 
 app.get('/usuarios', (req, res) => {
-    res.send({usuarios: ["jorge", "carlos", "Juan"]})
+    res.send({usuarios: ["jorge", "carlos", "Juan"]});
 });
 
 app.listen(3000);
